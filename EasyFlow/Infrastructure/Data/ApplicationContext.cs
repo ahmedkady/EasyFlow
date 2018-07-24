23f6eb1ca4b94217726fb78e0cae2789c39e0baf
Ahmed Abd ElRahman <ahmed_kady@outlook.com>
2018-07-24 19:04:57 +0200

    adding structure skeleton

---
+++ EasyFlow/Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions opts) : base(opts)
+        {
+        }
+         
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
